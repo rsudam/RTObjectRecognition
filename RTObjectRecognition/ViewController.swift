afa2b5e5ee9a9002c92ab4e030a1f26f48336948
rsudam <raghu.sairam@gmail.com>
2019-01-19 19:33:54 +0530

    Initial Commit

---
+++ RTObjectRecognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RTObjectRecognition
+//
+//  Created by Raghu Sairam on 19/01/19.
+//  Copyright © 2019 Raghu Sairam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
